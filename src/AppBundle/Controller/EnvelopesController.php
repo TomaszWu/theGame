6fd26805d1fbdcc029556fa3a11abcdaae17435b
TomaszWu <tomasz.wojciechowski84@gmail.com>
2017-02-14 18:12:25 +0100

    random value added, passing value by slug

--- src/AppBundle/Controller/EnvelopesController.php
+++ src/AppBundle/Controller/EnvelopesController.php
@@ -5,27 +5,40 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Envelopes;
+use AppBundle\Model\EnvelopeModel;
+use AppBundle\Model\GameModel;
+use AppBundle\Entity\Game;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EnvelopesController extends Controller {
 
     public function chooseTheEnvelopeAction(Request $request) {
-        $value = ['1', '2', '3', '4', '5'];
-
+        $envelopesColours = ['Czerwona', 'Zielona', 'Żółta', 'Niebieska', 'Różowa'];
+        shuffle($envelopesColours);
         $form = $this->createFormBuilder()
-                ->add('Koperta', 'choice', [
-                    'choices' => $value,
+                ->add('Koperty:', 'choice', [
+                    'choices' => ($envelopesColours),
                     'expanded' => true
                 ])
                 ->add('envelope', 'submit', array('label' => 'Wybierz kopertę'))
                 ->getForm();
-        
+
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
-            return new RedirectResponse($this->generateUrl('game_get_question'));
+            $gameId = $this->get('session')->get('gameId');
+            $game = $this->get('game.model')->getGameById($gameId);
+            $choosenEnvelope = new Envelopes;
+            $envelopeValue = $this->get('envelope.model')->determineTheValueOfTheEnvelope();
+            $choosenEnvelope->setValue($envelopeValue);
+            $this->get('envelope.model')->save($choosenEnvelope);
+            $game->addEnvelope($choosenEnvelope);
+            $this->get('game.model')->save($game);
+            return new RedirectResponse($this->generateUrl('game_get_question', 
+                    array('envelopeValue' => $envelopeValue)));
         }
 
-        return $this->render("@App/Envelopes/chooseTheEnvelope.html.twig", ['form' => $form->createView()]);
+        return $this->render("@App/Envelopes/chooseTheEnvelope.html.twig", 
+                ['form' => $form->createView()]);
     }
 
 }
